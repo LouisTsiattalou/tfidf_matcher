635b1303c00175799c46ded613984f16046e5638
Louis Tsiattalou <louis.tsi@gmail.com>
2019-10-06 16:22:44 +0100

    Adapt `ngrams()` from original scratch script

---
+++ tfidf_matcher/ngrams.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# TITLE: NGrams
+# AUTHOR: Louis Tsiattalou
+# DATE STARTED: Thu Oct 3 23:01:11 2019
+# GITLAB: https://gitlab.com/LouisTsiattalou/tfidf_matcher
+# DESCRIPTION:
+# Extract ngrams from a string.
+
+"""Generate a full list of ngrams from a list of strings
+
+:param string: List of strings to generate ngrams from.
+:type string: list (of strings)
+:param n: Maximum length of the n-gram. Defaults to 3.
+:type n: int
+...
+:raises AssertionError: If you pass in a list that contains
+datatypes other than `string`, you're in trouble!
+...
+:return: Returns list of ngrams generated from the input string.
+:rtype: list
+"""
+
+import re
+
+def ngrams(string, n=3):
+    # Assert string type
+    assert type(string) == type("string"), "String not passed in!"
+
+    # Remove Punctuation from the string
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+
+    # Generate zip of ngrams (n defined in function argument)
+    ngrams = zip(*[string[i:] for i in range(n)])
+
+    # Return ngram list
+    return [''.join(ngram) for ngram in ngrams]
