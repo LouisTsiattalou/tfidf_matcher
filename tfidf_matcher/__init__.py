dc53227be09f649f6f80203db4692d6d029bfb46
Louis Tsiattalou <louis.tsi@gmail.com>
2019-10-06 16:24:32 +0100

    Generate `__init__.py`

---
+++ tfidf_matcher/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# version as tuple for simple comparisons
+VERSION = (0, 1, 1)
+# string created from tuple to avoid inconsistency
+__version__ = ".".join([str(x) for x in VERSION])
+
+from tfidf_matcher.ngrams import ngrams
+from tfidf_matcher.matcher import matcher
