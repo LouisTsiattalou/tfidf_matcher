42e7b487ca7360e9e2239c1f71a9a967af177d7d
Louis Tsiattalou <louis.tsi@gmail.com>
2020-02-18 16:44:22 +0000

    Add tests covering `n` argument in ngrams

--- test/test_ngrams.py
+++ test/test_ngrams.py
@@ -7,16 +7,22 @@ from tfidf_matcher.ngrams import ngrams
 # import ngrams` and execute using `python -m unittest discover -s tests -t src`
 
 class TestNgrams(unittest.TestCase):
+
     def test_ngram_count(self):
         """Test that the number of ngrams returned by the function is correct."""
-        data = "Testing Number Of NGrams"
-        self.assertEqual(len(ngrams(data)), 22)
+        data = "This is a sentence"
+        self.assertEqual(len(ngrams(data, n=1)), 18)
+        self.assertEqual(len(ngrams(data, n=2)), 17)
+        self.assertEqual(len(ngrams(data, n=3)), 16)
+        self.assertEqual(len(ngrams(data, n=18)), 1)
 
     def test_ngram_length(self):
         """Test that the ngram outputs from the function always contain `n` length ngrams."""
-        n = 3
-        data = "Testing Number Of NGrams"
-        self.assertTrue(all([len(x) == n for x in ngrams(data)]))
+        data = "This is a sentence"
+        self.assertTrue(all([len(x) == 1 for x in ngrams(data, n=1)]))
+        self.assertTrue(all([len(x) == 2 for x in ngrams(data, n=2)]))
+        self.assertTrue(all([len(x) == 3 for x in ngrams(data, n=3)]))
+        self.assertTrue(all([len(x) == 18 for x in ngrams(data, n=18)]))
 
 if __name__ == "__main__":
     unittest.main()
