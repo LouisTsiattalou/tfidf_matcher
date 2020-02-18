9153c99ebc9d56a86ee7ecabcadf467ee310630f
Louis Tsiattalou <louis.tsi@gmail.com>
2020-02-18 16:32:54 +0000

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="tfidf_matcher-louistsiattalou",
     version="0.0.1",
     author="Louis Tsiattalou",
     author_email="louis.tsi@gmail.com",
-    description="A small package that enables super-fast TF-IDF based matching.",
+    description="A small package that enables super-fast TF-IDF based string matching.",
     long_description="TODO",
 #    install_requires=['pandas','numpy','re','sklearn'],
     long_description_content_type="text/markdown",
     url="https://github.com/louistsiattalou/tfidf_matcher",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=find_packages(),
     python_requires='>=3.0',
+    install_requires=["pandas>=0.23", "scikit-learn>=0.22"]
 )
