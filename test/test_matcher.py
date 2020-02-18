fbc98504fa14ca6e452a3943367712b6815dffd5
Louis Tsiattalou <louis.tsi@gmail.com>
2020-02-18 16:54:32 +0000

    Add Shape Tests for `matcher` output

--- test/test_matcher.py
+++ test/test_matcher.py
@@ -8,12 +8,17 @@ from tfidf_matcher.matcher import matcher
 # import ngrams` and execute using `python -m unittest discover -s tests -t src`
 
 class TestMatcher(unittest.TestCase):
+
+    test_list = ["hello", "theseare", "definitely", "companynames"]
+    test_lookup = ["hullo", "company", "finite", "cesare"]
+
     def test_matcher_output_shape(self):
-        """TODO Test that the output of matcher is as expected."""
-        pass
+        """Test that the output of matcher is as expected."""
+        for k in range(1, len(self.test_lookup)):
+            self.assertEqual(matcher(self.test_list, self.test_lookup, k_matches = k).shape, (len(self.test_list), k+2))
 
     def test_matcher_outputs_found_in_original(self):
-        """TODO"""
+        """TODO Test that all of the matcher outputs are found in the original"""
         pass
 
 if __name__ == "__main__":
