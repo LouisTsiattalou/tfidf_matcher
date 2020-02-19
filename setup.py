72aa211fbebb89cf8cf383f0821afd6130c3a5fe
Louis Tsiattalou <louis.tsi@gmail.com>
2020-02-19 16:38:24 +0000

    Fix issue with dependencies installing
    Needed to define them explicitly, and stop the automatic semantic versioning
    nonsense (it only lives in one place anyway, doesn't need to be defined in `__init__.py`)

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="tfidf_matcher-louistsiattalou",
-    version="0.0.1",
+    version="0.1.0",
     author="Louis Tsiattalou",
     author_email="louis.tsi@gmail.com",
     description="A small package that enables super-fast TF-IDF based string matching.",
     long_description="TODO",
-#    install_requires=['pandas','numpy','re','sklearn'],
     long_description_content_type="text/markdown",
     url="https://github.com/louistsiattalou/tfidf_matcher",
     classifiers=[
@@ -19,5 +18,6 @@ setup(
     ],
     packages=find_packages(),
     python_requires='>=3.0',
-    install_requires=["pandas>=0.23", "scikit-learn>=0.22"]
+    install_requires=["sklearn", "pandas"],
+    setup_requires=["sklearn", "pandas"]
 )
--- tfidf_matcher/__init__.py
+++ tfidf_matcher/__init__.py
@@ -4,8 +4,6 @@
 from .ngrams import ngrams
 from .matcher import matcher
 
-__all__ = ['ngrams','matcher']
-
 # version as tuple for simple comparisons
 VERSION = (0, 1, 1)
 # string created from tuple to avoid inconsistency
